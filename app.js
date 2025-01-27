b7762aa398da3b871b5338bbcabd6d9822d1d781
YamanNTG <claudiuoprea21@gmail.com>
2025-01-27 21:11:27 +0000

    Proxy setup

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const userRouter = require('./routes/userRoutes');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000,
